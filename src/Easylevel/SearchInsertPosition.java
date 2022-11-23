5473e353a2c7c18c6f837eddb4fa71ba949f1843
Tiyasbiswas <38094884+Tiyasbiswas@users.noreply.github.com>
2022-11-23 12:34:14 +0100

    Update SearchInsertPosition.java

--- src/Easylevel/SearchInsertPosition.java
+++ src/Easylevel/SearchInsertPosition.java
@@ -1,8 +1,5 @@
 package Easylevel;
 
-//Given a sorted array of distinct integers and a target value,
-//return the index if the target is found. If not, return the index where it would be if it were inserted in order.
-// You must write an algorithm with O(log n) runtime complexity.
 
 public class SearchInsertPosition{
 
